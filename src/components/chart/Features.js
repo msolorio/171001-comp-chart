c105b981a4edbc400ef67e30cf3078b5b21fe68f
msolorio <msolorio789@gmail.com>
2017-10-01 16:35:30 -0700

    set up ui scaffolding

---
+++ src/components/chart/Features.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+export default function Features(props) {
+  return (
+    <div className="Features">{props.children}</div>
+  );
+};
---
+++ src/components/chart/Headers.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+export default function Headers(props) {
+  return (
+    <div className="Headers">{props.children}</div>
+  );
+};
